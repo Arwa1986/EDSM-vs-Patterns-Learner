d176f94ee7a19ea3ba1b1412e877611d95f3ab73
Arwa1986 <aaalfitni1@sheffield.ac.uk>
2025-05-08 13:14:39 +0100

    consider sink-state as rejected-state

--- Apps/RandomWalksGenerator.py
+++ Apps/RandomWalksGenerator.py
@@ -7,8 +7,8 @@ from Transition import Transition
 
 
 def generate_random_walks(graph:Graph, max_length, positive_walks_size, negative_walks_size):
-    positive_walks = generate_Training_positive_walks(positive_walks_size, max_length, graph)
-    negative_walks = generate_prefixed_closed_negative_walks(negative_walks_size, max_length, graph)
+    positive_walks = generate_Training_positive_walks_sink_state_is_rejected(positive_walks_size, max_length, graph)
+    negative_walks = generate_prefixed_closed_negative_walks_sink_state_is_rejected(negative_walks_size, max_length, graph)
 
     return positive_walks, negative_walks
 
@@ -74,7 +74,7 @@ def generate_Training_positive_walks(positive_walks_size, max_length, graph):
             i += 1
     print(f'loop counter: {loop_counter}')
     return positive_walks
-
+# def generate_Training_positive_walks(positive_walks_size, max_length, graph):
 def generate_evaluation_positive_walks(positive_walks_size, max_length, graph):
     # positive walks that are not neccessarily cover all transitions and states
     positive_walks = []
@@ -129,6 +129,88 @@ def long_enough():
     random_float = random.uniform(0, 1)
     return random_float == 0.7
 
+def split_into_evaluation_and_training_lists(walks):
+    Evaluation_pos_walks = []
+    random_index_list = []
+    for i in range(29):
+        random_index = random.randint(0, len(walks) - 1)
+        if random_index not in random_index_list:
+            random_index_list.append(random_index)
+            # add the evaluation traces
+            Evaluation_pos_walks.append(walks[random_index])
+
+    Training_pos_walks = []
+    for i in range(len(walks) - 1):
+        # remove the evaluation traces
+        if i not in random_index_list:
+            Training_pos_walks.append(walks[i])
+
+    return Evaluation_pos_walks,Training_pos_walks
+
+def generate_Training_positive_walks_sink_state_is_rejected(positive_walks_size, max_length, graph):
+    positive_walks = []
+    i = 0
+    # transition_cover = False
+    # state_cover = False
+    # all_states = graph.get_all_states()
+    # all_transitions = set(graph.get_outgoing_transitions_for_list_of_states(None, all_states))
+    # discovered_transitions = set()
+    # discovered_states = []
+    loop_counter = 0
+    while i < positive_walks_size :#or (discovered_transitions!=all_transitions)
+        loop_counter += 1
+        walk_length = random.randint(1, max_length)
+        walk = []
+        current_state = graph.initial_state
+        while len(walk) < walk_length:
+            input = random.choice(graph.input_alphabet)
+            transition = graph.get_outgoing_transitions_for_state_with_input(current_state, input)
+            if transition.to_state.color == 'yellow': # sink state
+                continue
+            walk.append(transition.label)
+            # if transition not in discovered_transitions:
+            #     discovered_transitions.add(transition)
+            current_state = graph.get_target_state(current_state, input)
+            # j+=1
+        if walk not in positive_walks:
+            positive_walks.append(walk)
+            i += 1
+    print(f'loop counter: {loop_counter}')
+    return positive_walks
+
+def generate_prefixed_closed_negative_walks_sink_state_is_rejected(negative_walks_size, max_length, graph):
+    negative_walks = []
+    i = 0
+    while i < negative_walks_size:
+        walk_length = random.randint(1, max_length)
+        walk = []
+        current_state = graph.initial_state
+        for j in range(walk_length - 1):
+            input = random.choice(graph.input_alphabet)
+            # output = graph.get_output(current_state, input)
+            # transition = input + ' / ' + output
+            # walk.append(transition)
+            transition = graph.get_outgoing_transitions_for_state_with_input(current_state, input)
+            if transition.to_state.color == 'yellow':  # sink state
+                continue
+            walk.append(transition.label)
+            current_state = graph.get_target_state(current_state, input)
+        # generate the last transition that makes the walk negative. (add not exsitiing transition to the walk)
+        input = random.choice(graph.input_alphabet)
+        transition = graph.get_outgoing_transitions_for_state_with_input(current_state, input)
+        if transition.to_state.color == 'yellow':  # sink state
+            last_label = transition.label
+        else:
+            output = get_not_exist_output(current_state, input, graph)
+            last_label = input + '/' + output
+        walk.append(last_label)
+
+        if walk not in negative_walks:
+            negative_walks.append(walk)
+            i += 1
+
+    return negative_walks
+
 if __name__ == '__main__':
     # Create the state objects
     state0 = State(0)
@@ -165,21 +247,3 @@ if __name__ == '__main__':
     positive_walks = genetare_transition_cover_walks(G, 5, 5)
     for walk in positive_walks:
         print(walk)
-
-def split_into_evaluation_and_training_lists(walks):
-    Evaluation_pos_walks = []
-    random_index_list = []
-    for i in range(29):
-        random_index = random.randint(0, len(walks) - 1)
-        if random_index not in random_index_list:
-            random_index_list.append(random_index)
-            # add the evaluation traces
-            Evaluation_pos_walks.append(walks[random_index])
-
-    Training_pos_walks = []
-    for i in range(len(walks) - 1):
-        # remove the evaluation traces
-        if i not in random_index_list:
-            Training_pos_walks.append(walks[i])
-
-    return Evaluation_pos_walks,Training_pos_walks
